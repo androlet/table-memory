4035e974b13ee89a7944edb5eafaa61aa1467813
Thomas Lecomte <thomas.lecomte@parts-advisor.com>
2018-07-10 11:50:54 +0200

    add objects to list

--- src/app/data/objects.ts
+++ src/app/data/objects.ts
@@ -3,5 +3,8 @@ export const OBJECTS = [
   'médaille', 'carabine', 'chaussure', 'violon', 'gauffre', 'nénuphare', 'boue', 'ampoule', 'grenadine', 'virus',
   'seringue', 'cocinelle', 'phare', 'rivière', 'casque', 'souris', 'pyramide', 'blouson', 'sopalin', 'marmitte',
   'argent', 'maïs', 'ruban', 'cravate', 'livre', 'fil', 'classeur', 'trésor', 'lampe à huile', 'kangourou',
-  'café', 'silex', 'marche-pied', 'baudrier', 'brassard', 'ceinture', 'beurre', 'boussole', 'dentifrice', 'potiron'
+  'café', 'silex', 'marche-pied', 'baudrier', 'brassard', 'ceinture', 'beurre', 'boussole', 'dentifrice', 'potiron',
+  'poudre', 'truffe', 'vélo', 'étoile', 'sucre', 'porte-manteau', 'ordinateur', 'bouchon d\'oreille', 'clef',
+  'moineau', 'moine', 'tatouage', 'vis', 'passeport', 'ragoût', 'mouchoir', 'jardin', 'ballon', 'veine',
+  'coeur', 'papier', 'cheminée', 'tableau', 'casserole', 'perruque', 'quille', 'huitre', 'oeuf', 'AWS'
 ];
